28af1179ec702d2a3c31357bb4090524656f62ee
Sultan R <sultanr2409@gmail.com>
2023-11-28 19:43:13 +0700

    revisi technical test

--- app/login/page.js
+++ app/login/page.js
@@ -1,17 +1,40 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const page = () => {
-  const email = "test";
+  var time = new Date();
+  let hours = time.getHours();
+  const router = useRouter();
+
+  const item = JSON.parse(sessionStorage.getItem("data"));
+  const name = item.email.split("@");
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("data");
+    router.push("/");
+  };
+
   return (
     <div className="">
       <div className="bg-indigo-600 flex flex-row mx-auto justify-between">
-        <h2>{email}</h2>
+        <h2>{item.email.toLocaleUpperCase()}</h2>
         <div>
-          <Link href={"/"}>Logout</Link>
+          <button onClick={handleLogout}>Logout</button>
         </div>
       </div>
-
-      <h1>Selamat Siang</h1>
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        <h1>Halo, {name[0]}</h1>
+        <h1>{hours <= 18 ? "Selamat Siang" : "Selamat Malam"}</h1>
+        <h1>
+          {time.toLocaleString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          })}
+        </h1>
+      </div>
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,53 +1,70 @@
 "use client";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 export default function Home() {
-  const [form, setForm] = useState({ email: "", password: "" });
+  const router = useRouter();
 
-  // const router = useRouter();
-  const changeForm = (event) => {
-    setForm((data) => {
-      return {
-        ...data,
-        [event.target.name]: event.target.value,
-      };
-    });
-  };
-
-  const login = async () => {
-    // await router.push("/login");
-  };
+  const loginSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email")
+      .min(10, "too short")
+      .max(15, "too long")
+      .required("Required"),
+    password: Yup.string()
+      .required("This field is required")
+      .min(8, "Pasword must be 8 or more characters")
+      .matches(
+        /(?=.*[a-z])(?=.*[A-Z])\w+/,
+        "Password ahould contain at least one uppercase and lowercase character"
+      )
+      .matches(/\d/, "Password should contain at least one number")
+      .matches(
+        /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/,
+        "Password should contain at least one special character"
+      ),
+  });
 
   return (
     <main className="max-w-xl min-h-screen mx-auto flex justify-center items-center">
-      <form className="flex flex-col" onSubmit={login}>
-        <label className="top-10 absolute ">Halaman Login</label>
-        <label className="">Email</label>
-        <input
-          name="email"
-          type="email"
-          autoComplete="email"
-          onChange={changeForm}
-          className="border-2"
-          required
-        />
-        <label className="">Password</label>
-        <input
-          name="password"
-          type="password"
-          autoComplete="password"
-          onChange={changeForm}
-          className="border-2 gap-3"
-          required
-        />
-        <button className="bg-indigo-500 my-2">
-          <Link href={"/login"} className="">
-            Login
-          </Link>
-        </button>
-      </form>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={loginSchema}
+        onSubmit={(values) => {
+          console.log(values);
+          sessionStorage.setItem("data", JSON.stringify(values));
+          router.push("/login");
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form className="flex flex-col">
+            <label className="top-10 absolute ">Halaman Login</label>
+            <label className="">Email</label>
+            <Field
+              name="email"
+              type="email"
+              className="border-2  border-black rounded-md"
+            />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+            <label className="">Password</label>
+            <Field
+              name="password"
+              type="password"
+              className="border-2  border-black rounded-md"
+            />
+            {errors.password && touched.password ? (
+              <div>{errors.password}</div>
+            ) : null}
+            <button type="submit" className="bg-indigo-500 my-2">
+              Login
+            </button>
+          </Form>
+        )}
+      </Formik>
     </main>
   );
 }
